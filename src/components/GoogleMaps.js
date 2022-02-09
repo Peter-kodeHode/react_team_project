48cc9f69eb6bcae5c931312407a2e0a95fdfeeea
Peter-kodeHode <peter.kodehode@gmail.com>
2022-02-09 12:14:39 +0100

    First commit

---
+++ src/components/GoogleMaps.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function GoogleMaps() {
+  return (
+
+    <>
+    <div className="gmaps">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1971.1262928672631!2d5.325753551588993!3d60.393591881955686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x463cfea87e0215db%3A0x290717943bdf607!2sBergen.Works!5e0!3m2!1sno!2sno!4v1644401027529!5m2!1sno!2sno"
+      width="400"
+      height="300"
+      style={{border:0}}
+      allowFullScreen=""
+      loading="lazy"
+    ></iframe></div></>
+  );
+}
+
+export default GoogleMaps;
