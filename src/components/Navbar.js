c74862227e93ca27bb536095865084047fdf2bca
Peter-kodeHode <peter.kodehode@gmail.com>
2022-03-02 14:12:14 +0100

    Added some stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-import checkButton from "./CheckButton"
+
+import checkButton from "./CheckButton";
 const bell = process.env.PUBLIC_URL + "/images/Bell.png";
 const hamburger = process.env.PUBLIC_URL + "/images/hamburger.png";
 const navArrow = process.env.PUBLIC_URL + "/images/navArrow.png";
 
-
-
 function Navbar() {
+  const [navBarOpen, setNavBarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavBarOpen((prev) => !prev);
+  };
+
   return (
     <>
       <div className="navbar">
-        <ul>
-          <li>
+        <ul className={`menuNav ${navBarOpen ? " showMenu" : ""}`}>
+          <li className="navLinks">
+            <a href="/">lollol</a>
+          </li>
+          <li className="navbarImages">
             <img className="navArrow" src={navArrow} alt="navArrow" />
           </li>
-          <li>
+          <li className="navbarImages">
             <img className="bell" src={bell} alt="bell" />
           </li>
-          <li>
-              <img className="hamburger" src={hamburger} alt="hamburgermenu" onClick={(checkButton)}/>
+          <li className="navbarImages">
+            <img
+              className="hamburger"
+              src={hamburger}
+              alt="hamburgermenu"
+              onClick={handleToggle}
+            />
           </li>
         </ul>
       </div>
     </>
-
   );
-  
 }
 
 export default Navbar;
